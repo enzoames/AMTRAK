585b67e17ccc6e5b7fa3f55a34799dd9a461a20e
Enzo <e.amespizarro@gmail.com>
2017-04-30 00:34:39 -0400

    calculates segment and fare for trip

--- amtrak/amtrakmain/views.py
+++ amtrak/amtrakmain/views.py
@@ -24,62 +24,26 @@ def home(request):
     if request.method == 'POST':  # This statement is executed when the user clicks on 'buy ticket' button
         print request.POST
 
-        # COMPUTE HERE THE OTHER PARTS OF THE FORM THAT NEED TO BE FILLED OUT
-
-        userSelectedStartTrip = request.POST['trip_start_station']
-        userSelectedEndTrip = request.POST['trip_end_station']
-
-        print ('\n\nStart Station: ')
-        print(userSelectedStartTrip)
-        print type(userSelectedStartTrip)
-
-        print ('\n\nEnd Station: ')
-        print(userSelectedEndTrip)
-        print type(userSelectedEndTrip)
-
-        userStartTripStation = Station.objects.get(id=userSelectedStartTrip)
-        userEndTripStation = Station.objects.get(id=userSelectedEndTrip)
-
-        print "\n\n USER START STATION", userStartTripStation
-        print "\n\n USER END STATION", userEndTripStation
-
-        south_end = Segment.objects.get(seg_south_end=userStartTripStation)
-        north_end = Segment.objects.get(seg_north_end=userEndTripStation)
-
-        print "\n\n USER SOUTH END", south_end
-        print "\n\n USER NORTH END", north_end
-
-        # TODO: the seg_north_end, seg_south_end are not mathcing correctly, needs fixing.
+        # COMPUTING MISSING PARTS, TRIP_SEGMENT_START, TRIP_SEGMENT_END, TRIP_FARE, TRIP_TRAIN
+        calculated_trip_info = calculateRemainingParts(request.POST)
 
         if form.is_valid():  # Checks the validity of the from they submitted
-            print ('\n\n\nForm is Valid...\n\n\n')
             # create a form instance and populate it with data from the request (user input)
-
-            # form.data['segmet'] = computed data
-
             instanceTripTicket = form.save(commit=False)
+            instanceTripTicket.trip_segment_start = calculated_trip_info['TripSegmentStart']
+            instanceTripTicket.trip_segment_end = calculated_trip_info['TripSegmentEnd']
+            instanceTripTicket.trip_fare = calculated_trip_info['TripFare']
 
-            #instanceTripTicket.trip_segment_start =
-            # instanceTripTicket.trip_segment_end =
-            # instanceTripTicket.trip_fare =
-            # instanceTripTicket.trip_train =
+            #form.save()  # Save to Database
 
-            #instance_TicketTrip = TicketTrip.objects.raw()
-
-            #model = TicketTrip()
-
-            # Do something here if needed; Calculate other the remaining columns of the database
-
-            form.save()  # Save to Database
+            instanceTripTicket.save()
 
             # Updating context to be displayed
             context = {
                 'title': 'Thank you for booking a ticket',
             }
-
     else:
         title = 'Welcome'
-
         # Adding some context to home.html. It allows us to use some sort of object and bring that into our template
         context = {
             'title': title,
@@ -90,6 +54,70 @@ def home(request):
     return render(request, 'amtrakmain/home.html', context)
     # Moreover, whenever the user clicks the 'buy ticket' button, it sends a request back to this view. This file works
     # as a constant loop.
+
+
+def calculateRemainingParts(request_POST):
+    # We store the station id, but we need the station name...
+    userSelectedStartTrip = request_POST['trip_start_station']
+    userSelectedEndTrip = request_POST['trip_end_station']
+
+    print ('\n\nUser Start Station: ')
+    print(userSelectedStartTrip)
+    print type(userSelectedStartTrip)
+
+    print ('\n\nUser End Station: ')
+    print(userSelectedEndTrip)
+    print type(userSelectedEndTrip)
+
+    # Based on the station id that we obtained above we can grab the 'station_name' from Station table
+    UserStartTripStation = Station.objects.get(id=userSelectedStartTrip)
+    UserEndTripStation = Station.objects.get(id=userSelectedEndTrip)
+
+    print "\n\n USER START STATION NAME", UserStartTripStation
+    print "\n\n USER END STATION NAME", UserEndTripStation
+
+    Total_Fare = 0
+    segment_list = Segment.objects.all()  # grabs all the objects (rows) from table Segment
+    # print "length of list: ", len(segment_list) == 24
+
+    if UserStartTripStation.id < UserEndTripStation.id:  # Trip Heading North
+        # This is the segment where the passenger's trip starts
+        Trip_Segment_Start = Segment.objects.get(seg_south_end=UserStartTripStation)
+        # This is the segment where the passenger's trip ends
+        Trip_Segment_End = Segment.objects.get(seg_north_end=UserEndTripStation)
+
+        print "\n\n TRIP SEGMENT START", Trip_Segment_Start  # has id
+        print "\n\n TRIP SEGMENT END", Trip_Segment_End  # has id
+
+        # Now, need to calculate the fare based on the segments calculated above
+        for seg in range(len(segment_list)):
+            if segment_list[seg].id >= Trip_Segment_Start.id:
+                Total_Fare += segment_list[seg].seg_fare
+                print "\tFARE: ", Total_Fare
+            if segment_list[seg].id == Trip_Segment_End.id:
+                break
+
+    else:  # Trip heading south
+        Trip_Segment_Start = Segment.objects.get(seg_north_end=UserStartTripStation)
+        Trip_Segment_End = Segment.objects.get(seg_south_end=UserEndTripStation)
+
+        print "\n\n TRIP SEGMENT START", Trip_Segment_Start  # has id
+        print "\n\n TRIP SEGMENT END", Trip_Segment_End  # has id
+
+        for seg in xrange(len(segment_list), 0, -1):
+            if segment_list[seg-1].id <= Trip_Segment_Start.id:
+                Total_Fare += segment_list[seg-1].seg_fare
+            if segment_list[seg-1].id == Trip_Segment_End:
+                break
+
+    remaining_information = {
+        'TripSegmentStart': Trip_Segment_Start,
+        'TripSegmentEnd': Trip_Segment_End,
+        'TripFare': Total_Fare,
+        #'Triptrain': None,
+    }
+
+    return remaining_information
 
 
 

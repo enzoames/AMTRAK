fe1e3a21180fe1b3b84986059f6c22c7e9044a6d
Enzo <e.amespizarro@gmail.com>
2017-05-16 20:35:41 -0400

    finish writing code for populating database

--- src/amtrak/amtrakmain/management/commands/populate_db.py
+++ src/amtrak/amtrakmain/management/commands/populate_db.py
@@ -8,6 +8,8 @@ THIS IS WHERE WE POPULATE OUR DATABASE
 from django.core.management.base import BaseCommand
 from amtrakmain.models import *
 from datetime import date, datetime, time, timedelta
+from dateutil.relativedelta import relativedelta
+import calendar
 
 class Command(BaseCommand):
     args = '<foo bar ...>'
@@ -67,11 +69,10 @@ class Command(BaseCommand):
     def _populate_stopsat(self):
         # If train arrives at station A at 6am and leaves at 6:10 -> time delta is 10
         # Same for time_out deltas
+
         # Time Deltas for North Bound Trains
-        north_time_in_deltas = [10, 12, 16, 23, 30, 20, 35, 25, 12, 6, 18, 86, 23, 28, 24, 53, 21, 14, 12, 17, 39, 39,
-                                19, 5]
-        north_time_out_deltas = [6, 12, 16, 23, 30, 24, 31, 25, 12, 6, 77, 27, 23, 28, 43, 34, 21, 14, 12, 17, 39, 39,
-                                 19, 000]
+        north_time_in_deltas = [10, 12, 16, 23, 30, 20, 35, 25, 12, 6, 18, 86, 23, 28, 24, 53, 21, 14, 12, 17, 39, 39, 19, 5]
+        north_time_out_deltas = [6, 12, 16, 23, 30, 24, 31, 25, 12, 6, 77, 27, 23, 28, 43, 34, 21, 14, 12, 17, 39, 39, 19, 000]
 
         # Gets North Bound Trains
         trains_list = Train.objects.filter(train_direction=1)
@@ -84,35 +85,155 @@ class Command(BaseCommand):
             # call helper function
             final_time = self.calculateTime_IN_OUT(starting_dates_list[count], north_time_in_deltas, north_time_out_deltas)
             for station in station_list:
-                instance = StopsAt(sa_train=train, sa_station=station,
-                        sa_time_in=final_time[0][i], sa_time_out=final_time[1][i])
+                instance = StopsAt(sa_train=train, sa_station=station, sa_time_in=final_time[0][i],
+                                   sa_time_out=final_time[1][i])
                 if final_time[1][i] < final_time[0][i]:  # We reached the last stop of train - no sa_time_out
-                    instance = StopsAt(sa_train=train, sa_station=station,
-                                       sa_time_in=final_time[0][i])
+                    instance = StopsAt(sa_train=train, sa_station=station, sa_time_in=final_time[0][i])
+
                 instance.save()
                 i += 1
 
             count += 1
 
         # Time Deltas for South Bound Trains
-        south_time_in_deltas = [5, 19, 39, 39, 17, 12, 14, 21, 53, 24, 28, 23, 86, 18, 6, 12, 25, 35, 20, 30, 23, 16, 12, 10]
-        south_time_out_deltas = [19, 39, 39, 17, 12, 14, 21, 34, 43, 28, 23, 27, 77, 6, 12, 25, 31, 24, 30, 23, 16, 12, 6, 000]
+        south_time_in_deltas = [19, 39, 39, 17, 12, 14, 21, 53, 24, 28, 23, 27, 77, 6, 12, 25, 35, 20, 30, 23, 16, 12, 10, 5]
+        south_time_out_deltas = [15, 39, 39, 17, 12, 14, 21, 53, 43, 9, 23, 86, 18, 6, 12, 25, 35, 24, 30, 19, 16, 12, 10, 0]
 
         # Gets South Bound Trains
         trains_list = Train.objects.filter(train_direction=0)
         station_list = Station.objects.all()
 
+        starting_dates_list = self.generateDates()
+        count = 0
         for train in trains_list:
             i = 0
-            final_time = self.calculateTime_IN_OUT(starting_dates_list[count], north_time_in_deltas, north_time_out_deltas)
-            for station in reversed(station_list):
-                station_instance = Station.Objects.get
+            final_time = self.calculateTime_IN_OUT(starting_dates_list[count], south_time_in_deltas, south_time_out_deltas)
+            for station in reversed(station_list):  # start iterating from te back
+                instance_south = StopsAt(sa_train=train, sa_station=station, sa_time_in=final_time[0][i],
+                                         sa_time_out=final_time[1][i])
+                if final_time[1][i] < final_time[0][i]:  # We reached the last stop of train - no sa_time_out
+                    instance_south = StopsAt(sa_train=train, sa_station=station, sa_time_in=final_time[0][i])
 
-        # TODO: NOW THAT STATIONS CAN BE ITERATES BACKWARDS, WE CAN SAVE STOPSAT INSTANCE
+                instance_south.save()
+                i += 1
+
+            count += 1
+
+        print ("\n STOPSAT TABLE POPULATED SUCCESSFULLY\n")
+
+    # ===============================================
+    # =========== POPULATE SEGMENT TABLE ============
+    # ===============================================
+
+    def _populate_segement(self):
+
+        distance = [0, 10, 19, 10, 28, 37, 25, 30, 33, 12, 4, 10, 16, 17, 21, 17, 29, 16, 7, 8, 18, 25, 30, 11, 2, 0]
+        fare = [0, 15, 28.5, 15, 42, 55.5, 37.5, 45, 49.5, 18, 6, 15, 24, 25.5, 31.5, 25.5, 43.5, 24, 10.5, 12, 27, 37.5,
+                45, 16.5, 3, 0]
+
+        for i in range(1, 25):
+            j = i + 1
+            retdist = distance[i]
+            retfare = fare[i]
+            north = Station.objects.get(id=j)
+            south = Station.objects.get(id=i)
+            instance = Segment(seg_north_end=north, seg_south_end=south, seg_fare=retfare, seg_distance=retdist)
+            instance.save()
+
+        print ("\n SEGMENT TABLE POPULATED SUCCESSFULLY\n")
+
+    # =================================================
+    # =========== POPULATE SEATSFREE TABLE ============
+    # =================================================
+
+    def _populate_seatsfree(self):
+        # ===== For Trains going NORTH =====
+
+        train_list = Train.objects.filter(train_direction=1)
+        segment_list = Segment.objects.all()
+
+        # Filling up SeatsFree for just the month of June 2017
+        year = 2017
+        month = 6
+        dates = self.generateDatesOfMonth(year, month)  # 2017-06-01 . . . 2017-06-30
+
+        previous_day_out = self.generateDates()
+        i = 0
+
+        for train in train_list:
+            temp_date = previous_day_out[i]
+
+            instance_stack = []  # contains 30 objects at a time
+
+            for segment in segment_list:
+                stops_at_instance = StopsAt.objects.get(sa_train=train, sa_station=segment.seg_south_end)
+                exact_time = stops_at_instance.sa_time_in
+
+                for d in dates:
+                    current_full_date = datetime.combine(d.date(), exact_time)
+
+                    # Check stack for the previous instance
+                    for obj in instance_stack:  # find exact object in stack
+                        if obj.sf_date.date() == current_full_date.date() and obj.sf_train == stops_at_instance.sa_train:
+                            temp_date = obj.sf_date
+                            break
+
+                    if temp_date.time() > current_full_date.time():
+                        current_full_date = datetime.combine(d.date() + timedelta(days=1), exact_time)
+
+                    instance = SeatsFree(sf_segment=segment, sf_train=train, sf_date=current_full_date, sf_count=448)
+                    instance_stack.append(instance)
+                    instance.save()
+
+            del instance_stack[:]
+            i += 1
+
+        # ===== For Trains going SOUTH =====
+
+        south_train_list = Train.objects.filter(train_direction=0)
+        i = 0
+
+        for train in south_train_list:
+            temp_date = previous_day_out[i]
+
+            instance_stack = []  # contains 30 objects at a time
+
+            for segment in reversed(segment_list):
+                stops_at_instance = StopsAt.objects.get(sa_train=train, sa_station=segment.seg_north_end)
+                exact_time = stops_at_instance.sa_time_in
+
+                for d in dates:
+                    current_full_date = datetime.combine(d.date(), exact_time)
+
+                    # Check stack for the previous instance
+                    for obj in instance_stack:  # find exact object in stack
+                        if obj.sf_date.date() == current_full_date.date() and obj.sf_train == stops_at_instance.sa_train:
+                            temp_date = obj.sf_date
+                            break
+
+                    if temp_date.time() > current_full_date.time():
+                        current_full_date = datetime.combine(d.date() + timedelta(days=1), exact_time)
+
+                    instance = SeatsFree(sf_segment=segment, sf_train=train, sf_date=current_full_date, sf_count=448)
+                    instance_stack.append(instance)
+                    instance.save()
+
+            del instance_stack[:]
+            i += 1
+
+        print ("\n SEATSFREE TABLE POPULATED SUCCESSFULLY\n")
 
     # ========================================
     # =========== HELPER FUNCTIONS ===========
     # ========================================
+
+    def generateDatesOfMonth(self, year, month):
+        # gets the number of days in the month and year specified
+        num_days = calendar.monthrange(year, month)[1]
+        # obtain a day object for each day in the month
+        days = [datetime(year, month, day) for day in range(1, num_days + 1)]
+
+        return days
 
     def generateDates(self):
         # Both Directions
@@ -155,32 +276,8 @@ class Command(BaseCommand):
     # =======================================
 
     def handle(self, *args, **options):
-        # self._populate_station() ALREADY POPULATED NO NEED TO RUN AGAIN
-        # self._populate_trains() ALREADY POPULATED NO NEED TO RUN AGAIN
+        # self._populate_station() ALREADY POPULATED NO NEED TO RUN AGAIN mysql
+        # self._populate_trains() ALREADY POPULATED NO NEED TO RUN AGAIN mysql
         self._populate_stopsat()
-        # self._populate_segement()
-
-
-
-
-
-
-
-
-
-# time_in = ["06:00:00", "06:10:00", "06:22:00", "06:38:00", "07:01:00", "07:31:00", "07:51:00", "08:26:00",
-#            "08:51:00", "09:03:00", "09:09:00", "09:27:00", "10:53:00", "11:16:00", "11:44:00", "12:08:00",
-#            "13:01:00", "13:22:00", "13:36:00", "13:48:00", "14:05:00", "14:44:00", "15:23:00", "15:42:00",
-#            "15:47:00"]
-#
-# time_out = ["06:05:00", "06:11:00", "06:23:00", "06:39:00", "07:02:00", "07:32:00", "07:56:00", "08:27:00",
-#             "08:52:00", "09:04:00", "09:10:00", "10:27:00", "10:54:00", "11:17:00", "11:45:00", "12:28:00",
-#             "13:02:00", "13:23:00", "13:37:00", "13:49:00", "14:06:00", "14:45:00", "15:24:00", "15:43:00"]
-
-
-
-        # Another approach, might be useful for different scenario
-        # minutes = lambda s, e: (s + datetime.timedelta(minutes=x) for x in xrange((e - s).seconds / 60 + 1))
-        #
-        # for m in minutes(today, today + datetime.timedelta(minutes=time_in_deltas[i])):
-        #     print m.time
+        #self._populate_segement()
+        #self._populate_seatsfree()
--- src/amtrak/amtrakmain/tests.py
+++ src/amtrak/amtrakmain/tests.py
@@ -5,40 +5,109 @@ from django.test import TestCase
 
 from datetime import date, datetime, time, timedelta
 
-# time_in = ["06:00:00", "06:10:00", "06:22:00", "06:38:00", "07:01:00", "07:31:00", "07:51:00", "08:26:00",
-#            "08:51:00", "09:03:00", "09:09:00", "09:27:00", "10:53:00", "11:16:00", "11:44:00", "12:08:00",
-#            "13:01:00", "13:22:00", "13:36:00", "13:48:00", "14:05:00", "14:44:00", "15:23:00", "15:42:00",
-#            "15:47:00"]
+# # time_in = ["06:00:00", "06:10:00", "06:22:00", "06:38:00", "07:01:00", "07:31:00", "07:51:00", "08:26:00",
+# #            "08:51:00", "09:03:00", "09:09:00", "09:27:00", "10:53:00", "11:16:00", "11:44:00", "12:08:00",
+# #            "13:01:00", "13:22:00", "13:36:00", "13:48:00", "14:05:00", "14:44:00", "15:23:00", "15:42:00",
+# #            "15:47:00"]
+# #
+# # time_out = ["06:05:00", "06:11:00", "06:23:00", "06:39:00", "07:02:00", "07:32:00", "07:56:00", "08:27:00",
+# #             "08:52:00", "09:04:00", "09:10:00", "10:27:00", "10:54:00", "11:17:00", "11:45:00", "12:28:00",
+# #             "13:02:00", "13:23:00", "13:37:00", "13:49:00", "14:06:00", "14:45:00", "15:24:00", "15:43:00"]
 #
-# time_out = ["06:05:00", "06:11:00", "06:23:00", "06:39:00", "07:02:00", "07:32:00", "07:56:00", "08:27:00",
-#             "08:52:00", "09:04:00", "09:10:00", "10:27:00", "10:54:00", "11:17:00", "11:45:00", "12:28:00",
-#             "13:02:00", "13:23:00", "13:37:00", "13:49:00", "14:06:00", "14:45:00", "15:24:00", "15:43:00"]
+# starting_date = datetime(2017, 6, 1, 06, 00)
+# starting_time = datetime(2017, 6, 1, 06, 00).time()
+# time_in_deltas = [10, 12, 16, 23, 30, 20, 35, 25, 12, 6, 18, 86, 23, 28, 24, 53, 21, 14, 12, 17, 39, 39, 19, 5]
+# final_time_in_values = [starting_date]
+# starting_date2 = datetime(2017, 6, 1, 06, 05)
+# starting_time2 = datetime(2017, 6, 1, 06, 05).time()
+# time_out_deltas = [6, 12, 16, 23, 30, 24, 31, 25, 12, 6, 77, 27, 23, 28, 43, 34, 21, 14, 12, 17, 39, 39, 19, 000]
+# final_time_out_values = [starting_date2]
+#
+# for i in range(len(time_in_deltas)):
+#     temp_time = datetime.combine(starting_date, starting_time) + timedelta(minutes=time_in_deltas[i])
+#     final_time_in_values.append(temp_time)
+#     starting_date = temp_time
+#     starting_time = starting_date.time()
+#
+#     temp_time2 = datetime.combine(starting_date2, starting_time2) + timedelta(minutes=time_out_deltas[i])
+#     final_time_out_values.append(temp_time2)
+#     starting_date2 = temp_time2
+#     starting_time2 = starting_date2.time()
+#
+# for t in final_time_in_values:
+#     print t.time()
+#
+# print " ====== "
+#
+# for t in final_time_out_values:
+#     print t.time()
 
-starting_date = datetime(2017, 6, 1, 06, 00)
-starting_time = datetime(2017, 6, 1, 06, 00).time()
-time_in_deltas = [10, 12, 16, 23, 30, 20, 35, 25, 12, 6, 18, 86, 23, 28, 24, 53, 21, 14, 12, 17, 39, 39, 19, 5]
-final_time_in_values = [starting_date]
-starting_date2 = datetime(2017, 6, 1, 06, 05)
-starting_time2 = datetime(2017, 6, 1, 06, 05).time()
-time_out_deltas = [6, 12, 16, 23, 30, 24, 31, 25, 12, 6, 77, 27, 23, 28, 43, 34, 21, 14, 12, 17, 39, 39, 19, 000]
-final_time_out_values = [starting_date2]
 
-for i in range(len(time_in_deltas)):
-    temp_time = datetime.combine(starting_date, starting_time) + timedelta(minutes=time_in_deltas[i])
-    final_time_in_values.append(temp_time)
-    starting_date = temp_time
-    starting_time = starting_date.time()
+#
+# def calculateTime_IN_OUT(starting_date, time_in_deltas, time_out_deltas):
+#     # starting_date => 2017-06-01 06:00:00
+#     starting_time = starting_date.time()  # 06:00:00
+#     final_time_in_values = [starting_date]  # list contains starting date
+#
+#     # Calculating time_out
+#     starting_date2 = datetime.combine(starting_date, starting_time) + timedelta(minutes=5)
+#     starting_time2 = starting_date2.time()  # 06:05:00
+#     final_time_out_values = [starting_date2]
+#
+#     for i in range(len(time_in_deltas)):
+#         temp_time = datetime.combine(starting_date, starting_time) + timedelta(minutes=time_in_deltas[i])
+#         final_time_in_values.append(temp_time)
+#         starting_date = temp_time
+#         starting_time = starting_date.time()
+#
+#         temp_time2 = datetime.combine(starting_date2, starting_time2) + timedelta(minutes=time_out_deltas[i])
+#         final_time_out_values.append(temp_time2)
+#         starting_date2 = temp_time2
+#         starting_time2 = starting_date2.time()
+#
+#     final = [final_time_in_values, final_time_out_values]
+#     return final
+#
+#
+# south_time_in_deltas = [19, 39, 39, 17, 12, 14, 21, 53, 24, 28, 23, 27, 77, 6, 12, 25, 35, 20, 30, 23, 16, 1, 10]
+# south_time_out_deltas = [15, 39, 39, 17, 12, 14, 21, 53, 43, 9, 23, 86, 18, 6, 12, 25, 35, 24, 30, 19, 16, 12, 0]
+#
+#
+#
+# #north_time_in_deltas = [10, 12, 16, 23, 30, 20, 35, 25, 12, 6, 18, 86, 23, 28, 24, 53, 21, 14, 12, 17, 39, 39, 19, 5]
+# #north_time_out_deltas = [6, 12, 16, 23, 30, 24, 31, 25, 12, 6, 77, 27, 23, 28, 43, 34, 21, 14, 12, 17, 39, 39, 19, 000]
+#
+# starting_date = datetime(2017, 6, 1, 06, 00)
+#
+#
+# final2 = calculateTime_IN_OUT(starting_date, south_time_in_deltas, south_time_out_deltas)
+#
+# #final2 = calculateTime_IN_OUT(starting_date, north_time_in_deltas, north_time_out_deltas)
+#
+#
+# for d in range(len(final2[0])):
+#     print final2[0][d].time(), final2[1][d].time()
+# # print "======="
+# # for d2 in final2[1]:
+# #     print d2.time()
 
-    temp_time2 = datetime.combine(starting_date2, starting_time2) + timedelta(minutes=time_out_deltas[i])
-    final_time_out_values.append(temp_time2)
-    starting_date2 = temp_time2
-    starting_time2 = starting_date2.time()
+# import datetime, calendar
+#
+# starting_date = datetime.datetime(2017, 6, 1, 06, 00)
+#
+# year = 2017
+# month = 2
+# num_days = calendar.monthrange(year, month)[1]
+# print num_days
+# days = [datetime.date(year, month, day) for day in range(1, num_days + 1)]
+#
+# for day in days:
+#     print day
 
-for t in final_time_in_values:
-    print t.time()
+previous_day = datetime(2017, 6, 1, 21, 00)
 
-print " ====== "
+exact_time = previous_day.time()
 
-for t in final_time_out_values:
-    print t.time()
+current_full_date = datetime.combine(previous_day.date() + timedelta(days=1), exact_time)
 
+print current_full_date
